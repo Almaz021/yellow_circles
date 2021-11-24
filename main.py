9d7c3c885d9d32c2f8f49108e26bb2f1d7bc4839
Almaz <almaz.kzn2005@yandex.ru>
2021-11-24 17:54:29 +0300

    Yellow circles

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from random import randint
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.do_paint = False
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        r = randint(1, 240)
+        qp.drawEllipse(100, 100, r, r)
+        self.do_paint = False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
